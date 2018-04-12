0a03293b1f308e876028d1a03fbec30cd6d06b36
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-12 05:08:20 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,11 @@
 class Appointment
+  attr_accessor :doctor
    attr_reader :doctor, :patient
 
    def initialize(date, doctor)
      @date = date
      @doctor = doctor
-     
+
    end
 
    def patient
